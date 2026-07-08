6f25540160c3edae40c2174b397f1638c2e64617
Miapelona2.0 <beserramia@gmail.com>
2026-07-07 21:42:25 -0300

    Add files via upload

---
+++ ej. 16/e16.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+
+imagen = Image.open("san_agusto.jpg").convert("RGB")
+
+def escala_grises(img):
+    ancho, alto = img.size
+    nueva = Image.new("L", (ancho, alto))
+
+    for i in range(ancho):
+        for j in range(alto):
+            R, G, B = img.getpixel((i, j))
+
+            gris = int(R * 0.2989 + G * 0.5870 + B * 0.1140)
+
+            nueva.putpixel((i, j), gris)
+
+    return nueva
+
+imagen_gris = escala_grises(imagen)
+
+plt.figure(figsize=(8,4))
+
+plt.subplot(1,2,1)
+plt.imshow(imagen)
+plt.title("Imagen Original")
+plt.axis("off")
+
+plt.subplot(1,2,2)
+plt.imshow(imagen_gris, cmap="gray")
+plt.title("Escala de Grises")
+plt.axis("off")
+
+plt.show()
